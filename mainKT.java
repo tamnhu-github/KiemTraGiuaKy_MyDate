06e43b373a4c600e59d82477fd36a2d5b2ebcffa
Trương Nguyễn Tâm Như <97598854+tamnhu-github@users.noreply.github.com>
2023-03-17 15:34:09 +0700

    MyDate

--- mainKT.java
+++ mainKT.java
@@ -3,12 +3,12 @@ public class mainKT {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		MyDate initDate = new MyDate(2001,2,29);
+		MyDate initDate = new MyDate(2001,2,29); //Nam khong nhuan: thang 2 co 29 ngay -> loi
 		System.out.println("Check error date: " +initDate.toString());
-		initDate.previousDay();
+		initDate.previousDay(); // 28/2/2001 -> hop le
 		System.out.println("\nCheck correct date: " +initDate.toString());
 		if (MyDate.isValidDate(initDate.getYear(),initDate.getMonth() ,initDate.getDay())) {
-			MyDate testDate = initDate.nextDay();
+			MyDate testDate = initDate.nextDay(); //1/3/2001
 			System.out.println("The next day: " + testDate.toString());
 		}
 	}
